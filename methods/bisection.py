a863033fb80e89c77031729fb52e4c67ea2eb69e
Syed Shah Meer Haider <CyBeR1337x@users.noreply.github.com>
2024-04-22 23:46:47 +0500

    Update bisection.py

--- methods/bisection.py
+++ methods/bisection.py
@@ -1,17 +1,17 @@
 
 from math import e, sin
 
-f = lambda x: e ** x * sin(x ** 2) - 10
+f = lambda x: x ** 3 + 4 * x ** 2 - 10
 
-x0 = 7
-x1 = 8
+x0 = 1
+x1 = 2
 x2 = (x0 + x1) / 2
 
 fx0, fx1, fx2  = f(x0), f(x1), f(x2)
 
 print("n", "x(n-1)", "x(n)", "x(n+1)", "f(xn+1)", "Diff", sep="\t\t")
 print("-"*100)
-print(0, x0, x1, x2, fx2, "-", sep='\t')
+
 
 diff = 0
 for i in range(1, 11):
